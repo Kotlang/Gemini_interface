061be2645bd34ac77fc26e03b251a0d34e375ee6
Lauquik <laukik.22010776@viit.ac.in>
2023-12-31 16:58:17 +0530

    past history fixed

--- app_chat.py
+++ app_chat.py
@@ -36,28 +36,41 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-# Sidebar allows a list of past chats
+def load_chat_history(chat_id):
+    try:
+        st.session_state.messages = joblib.load(f'data/{chat_id}-st_messages')
+        st.session_state.gemini_history = joblib.load(f'data/{chat_id}-gemini_messages')
+    except FileNotFoundError:
+        st.session_state.messages = []
+        st.session_state.gemini_history = []
+
+
+def start_new_chat():
+    st.session_state.chat_id = new_chat_id
+    st.session_state.messages = []
+    st.session_state.gemini_history = []
+
+
+def generate_chat_title(input_text):
+    words = input_text.split()
+    # Use the first four words or the entire text if it's shorter
+    return ' '.join(words[:4])
+
+
 with st.sidebar:
-    st.write('# Past Chats')
-    if st.session_state.get('chat_id') is None:
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id] + list(past_chats.keys()),
-            format_func=lambda x: past_chats.get(x, 'New Chat'),
-            placeholder='_',
-        )
-    else:
-        # This will happen the first time AI response comes in
-        st.session_state.chat_id = st.selectbox(
-            label='Pick a past chat',
-            options=[new_chat_id, st.session_state.chat_id] + list(past_chats.keys()),
-            index=1,
-            format_func=lambda x: past_chats.get(x, 'New Chat' if x != st.session_state.chat_id else st.session_state.chat_title),
-            placeholder='_',
-        )
-    # Save new chats after a message has been sent to AI
-    # TODO: Give user a chance to name chat
-    st.session_state.chat_title = f'ChatSession-{st.session_state.chat_id}'
+    st.write('# Chat Sessions')
+    # New Chat button
+    if st.button('New Chat'):
+        start_new_chat()
+        
+    for chat_id, chat_title in past_chats.items():
+        if st.sidebar.button(chat_title):
+            # Load the selected chat
+            st.session_state.chat_id = chat_id
+            st.session_state.chat_title = chat_title
+            load_chat_history(chat_id)
+            
+
 
 st.write('# Chat with Gemini')
 
@@ -69,11 +82,9 @@ try:
     st.session_state.gemini_history = joblib.load(
         f'data/{st.session_state.chat_id}-gemini_messages'
     )
-    print('old cache')
 except:
     st.session_state.messages = []
     st.session_state.gemini_history = []
-    print('new_cache made')
 st.session_state.model = genai.GenerativeModel('gemini-pro')
 st.session_state.chat = st.session_state.model.start_chat(
     history=st.session_state.gemini_history,
@@ -90,19 +101,12 @@ for message in st.session_state.messages:
 # React to user input
 if prompt := st.chat_input('Your message here...'):
     # Save this as a chat for later
-    if st.session_state.chat_id not in past_chats.keys():
-        past_chats[st.session_state.chat_id] = st.session_state.chat_title
-        joblib.dump(past_chats, 'data/past_chats_list')
+    st.session_state.chat_id = st.session_state.chat_id or new_chat_id
+
     # Display user message in chat message container
     with st.chat_message('user'):
         st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append(
-        dict(
-            role='user',
-            content=prompt,
-        )
-    )
+
     ## Send message to AI
     response = st.session_state.chat.send_message(
         prompt,
@@ -128,6 +132,18 @@ if prompt := st.chat_input('Your message here...'):
         # Write full message with placeholder
         message_placeholder.write(full_response)
 
+
+    # Add user message to chat history
+    if st.session_state.chat_id not in past_chats.keys():
+        past_chats[st.session_state.chat_id] = generate_chat_title(prompt)
+        joblib.dump(past_chats, 'data/past_chats_list')
+        
+    st.session_state.messages.append(
+        dict(
+            role='user',
+            content=prompt,
+        )
+    )
     # Add assistant response to chat history
     st.session_state.messages.append(
         dict(
